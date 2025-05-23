cfa4acc09e4e36b2b106c8fa76d764d2919af60c
shahzodjabbarov <jabborovshaxzod2002@gmail.com>
2025-05-23 22:26:49 +0900

    saved passwords

--- sample_app.py
+++ sample_app.py
@@ -1,4 +1,4 @@
-# Add to this file for the sample app lab
+# Add to this file for the sample app lab  id: 55e401f7bdcd password:  62955146070f4db9864d8b2648a99621  root@4671de257ed2
 from flask import Flask
 from flask import request
 from flask import render_template
